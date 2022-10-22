600bf58556299e4d281ff10c11638ef40b262174
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-10-22 12:56:13 +0200

    ejercicio 3 hecho

--- Jest exercises/src/__tests__/03-exercise.test.js
+++ Jest exercises/src/__tests__/03-exercise.test.js
@@ -8,7 +8,8 @@ describe("03-exercises", () => {
     const numbers = [1, 2, 3, 4, 5];
     const expected = [2, 4, 6, 8, 10];
 
-    expect.assertions(1);
+    const result = multiplyNums(numbers)
+    expect(result).toEqual(expected);
 
     /**
      * Make an assertion that the result of executing the multiplyNums function
@@ -21,7 +22,8 @@ describe("03-exercises", () => {
   test("multiplyNums doesn't mutate the original array", () => {
     const numbers = [1, 2, 3, 4, 5];
 
-    expect.assertions(1);
+    const result = multiplyNums(numbers)
+    expect(result).not.toEqual(numbers);
 
     /**
      * Write an assertion that the array returned by the multiplyNums function
@@ -45,9 +47,13 @@ describe("03-exercises", () => {
       ...safeUserData,
       password: "a98dsj9a8sdj89asd89jasd",
       role: "ADMIN",
+
     };
 
-    expect.assertions(1);
+
+    const result = sanitizeUserData(userWithSensitiveInformation);
+
+    expect(result).toEqual(safeUserData);
 
     /**
      * Finish the test so that it checks if the result of calling
