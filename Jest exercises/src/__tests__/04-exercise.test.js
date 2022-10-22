6a2b12fafdb9ac5555156f498edbe8a5dd9b5aaa
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-10-22 14:10:01 +0200

    ejercicio 4 hecho

--- Jest exercises/src/__tests__/04-exercise.test.js
+++ Jest exercises/src/__tests__/04-exercise.test.js
@@ -9,7 +9,14 @@ import {
  */
 describe("04-exercises", () => {
   test("createUserObject returns a user object with the right properties", () => {
-    expect.assertions(3);
+    
+
+    const result = createUserObject("Alex", "Spence", "alex@mail.com");
+
+      expect(result.firstName).toBeTruthy();
+      expect(result.lastName).toBeTruthy();
+      expect(result.email).toBeTruthy();
+    
 
     /**
      * Write 3 assertions that check if executing the createUserObject function
@@ -35,7 +42,11 @@ describe("04-exercises", () => {
   });
 
   test("createCorporateEmailAddress appends the corporate email domain", () => {
-    expect.assertions(1);
+     const result = createCorporateEmailAddress("dani");
+     expect(result).toMatch(/@company.com/);
+  });
+    
+  
 
     /**
      * Write an assertion using the `.toMatch()` matcher that the result
@@ -45,10 +56,14 @@ describe("04-exercises", () => {
      */
 
     // Write the assertion
-  });
+    
+    
+
 
   test("generateRandomNumberFrom1to10 returns a number between 1 and 10", () => {
-    expect.assertions(2);
+    const result = generateRandomNumberFrom1to10 ();
+    expect(result).toBeGreaterThan(0);
+    expect(result).toBeLessThan(11);
 
     /**
      * Write 2 assertions to check that the generateRandomNumberFrom1to10 function
